05262803e2f909330dfeebf8f0c79dc0126f7343
Xavier.Wang <xavierpe@qq.com>
2022-07-12 21:52:06 +0800

    HyggeProperties 细微调整

---
+++ hygge-commons-spring-boot/src/main/java/hygge/commons/spring/configuration/HyggeSpringProperties.java
@@ -0,0 +1,12 @@
+package hygge.commons.spring.configuration;
+
+/**
+ * 标识当前类是个托管给 Spring 自动绑定的配置项类<br/>
+ * 便于方便找到其同类的兄弟类
+ *
+ * @author Xavier
+ * @date 2022/7/11
+ * @since 1.0
+ */
+public interface HyggeSpringProperties {
+}
