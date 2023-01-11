af2a4aa0b5f60056fed9260c2347df3b20a793eb
Xavier.Wang <xavierpe@qq.com>
2023-01-12 01:24:32 +0800

    调整 AutoLogController 的 javadoc

--- hygge-web-toolkit/src/main/java/hygge/web/template/AutoLogController.java
+++ hygge-web-toolkit/src/main/java/hygge/web/template/AutoLogController.java
@@ -1,10 +1,13 @@
 package hygge.web.template;
 
+import hygge.web.utils.log.ControllerLogPointCut;
+
 /**
  * 自动记录出入参日志的 Controller
  *
  * @author Xavier
  * @date 2022/11/16
+ * @see ControllerLogPointCut
  */
 public interface AutoLogController {
 }
