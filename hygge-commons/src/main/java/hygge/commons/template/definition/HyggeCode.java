be6783cc8473126cd6ab0b536820425c2f81c223
Xavier.Wang <xavierpe@qq.com>
2026-04-01 10:31:56 +0800

    完善 HyggeCode 注释

--- hygge-commons/src/main/java/hygge/commons/template/definition/HyggeCode.java
+++ hygge-commons/src/main/java/hygge/commons/template/definition/HyggeCode.java
@@ -16,8 +16,11 @@
 
 package hygge.commons.template.definition;
 
+import hygge.commons.exception.main.HyggeException;
+import hygge.commons.exception.main.HyggeRuntimeException;
+
 /**
- * 业务码
+ * 业务码模板
  *
  * @author Xavier
  * @date 2022/6/25
@@ -25,22 +28,25 @@ package hygge.commons.template.definition;
  */
 public interface HyggeCode {
     /**
-     * code 值是否允许重复
+     * code 值是否允许重复，设置为 true 时，自身不参与 code 值冲突检查，不会被其他业务码作为比较对象
+     *
+     * @see hygge.commons.spring.validator.impl.HyggeCodeUniqueValidator#validate
      */
     default boolean isCodeDuplicateEnable() {
         return false;
     }
 
     /**
-     * 是否为严重问题。
-     * <p/>
-     * 返回 false 通常代表是入参引发的问题，有可能通过重试自愈<br/>
-     * 返回 true 时需要人工介入检查，通常无法自愈
+     * 当前业务码对应的情景是否发生了严重问题。
+     * <br/>
+     * <br/>
+     * 返回 false 通常代表：①无异常，正常流程、②预期之内的情景，请求方通过调整请求参数重试即可自愈<br/>
+     * 返回 true 通常代表：需要人工介入检查，通常出现了无法自愈问题
      */
     boolean isSerious();
 
     /**
-     * 获取已脱敏可暴露给系统外部的提示信息
+     * 获取已脱敏可暴露给系统外部请求发起者的提示信息
      */
     String getPublicMessage();
 
@@ -51,6 +57,10 @@ public interface HyggeCode {
 
     /**
      * 用于自由拓展的额外的信息，你可以通过该属性向 HyggeCode 的接收者传递某些信息
+     *
+     * @see HyggeException
+     * @see HyggeRuntimeException
+     * @see hygge.web.template.definition.HyggeBaseController
      */
     <E> E getExtraInfo();
 }
